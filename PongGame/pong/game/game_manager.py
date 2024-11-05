d6e56853aa7cf0918427f19e0d2d88172b121094
Pascal Gouasmi <pgouasmi@student.42lyon.fr>
2024-11-05 14:21:14 +0100

    added logs to check whereas it a new game or joining one

--- PongGame/pong/game/game_manager.py
+++ PongGame/pong/game/game_manager.py
@@ -12,40 +12,14 @@ class GameManager:
 
     async def create_or_get_game(self, game_id: str) -> GameWrapper:
         async with self._lock:
-            logging.info(f"Creating or getting game with id: {game_id}")
+            # logging.info(f"Creating or getting game with id: {game_id}")
             if game_id not in self.active_games:
+                logging.info(f"Game CREATED with id: {game_id}")
                 self.active_games[game_id] = GameWrapper(game_id)
-                logging.info(f"Game created with id: {game_id}")
+                # logging.info(f"Game created with id: {game_id}")
+            else:
+                logging.info(f"Game RETRIEVED with id: {game_id}")
             return self.active_games[game_id]
 
 # Instance unique
 game_manager = GameManager()
-
-# class GameManager:
-#     def __init__(self):
-#         self.active_games = {}  # uid: GameWrapper
-#         self._lock = asyncio.Lock()
-#
-#     async def create_game(self, game_id: str) -> GameWrapper:
-#         async with self._lock:
-#             if game_id not in self.active_games:
-#                 self.active_games[game_id] = GameWrapper(game_id)
-#             return self.active_games[game_id]
-#
-#     async def remove_game(self, game_id: str):
-#         async with self._lock:
-#             if game_id in self.active_games:
-#                 del self.active_games[game_id]
-#
-#     async def get_game(self, game_id: str) -> Optional[GameWrapper]:
-#         return self.active_games.get(game_id)
-#
-#     async def cleanup_stale_games(self):
-#         while True:
-#             async with self._lock:
-#                 current_time = datetime.now()
-#                 for game_id, game in list(self.active_games.items()):
-#                     if (game.status == GameStatus.WAITING and
-#                             (current_time - game.created_at).seconds > 60):
-#                         await self.remove_game(game_id)
-#             await asyncio.sleep(30)
