7842882a539ff859378365e92a3545ccf981ff14
Timothee Gayet <tgayet@student.42lyon.fr>
2024-11-18 12:27:12 +0100

    changed numbers of points for a match

--- PongGame/pong/game/game.py
+++ PongGame/pong/game/game.py
@@ -52,7 +52,7 @@ class Game:
         # self.init_ai()
 
         # game related variables
-        self.scoreLimit = 1000000000
+        self.scoreLimit = 2
         self.run = True
         self.pause = False
         self.goal1 = False
